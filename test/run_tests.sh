ece12602cd236ec79f096ae85e2460ef44054ba1
Tom Schumm <fwiffo@gmail.com>
2018-07-04 16:30:30 -0700

    TODOs for tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,11 +4,25 @@
 ./multifinder -e 10B | sort -bn > /tmp/multifinder_all.txt
 diff /tmp/multifinder_all.txt test/golden_all.txt
 
+# TODO: start / end seed
+
 # Basic output with threads
 mkdir /tmp/multifinder_thread
 ./multifinder -e 20B -t 4 -o /tmp/multifinder_thread
 cat /tmp/multifinder_thread/*.txt | sort -bn > /tmp/multifinder_thread.txt
 diff /tmp/multifinder_thread.txt test/golden_thread.txt
+
+# TODO: append
+
+# TODO: all biomes
+# TODO: plentiful biomes
+# TODO: spawn biomes
+
+# TODO: monuments
+# TODO: strongholds
+# TODO: woodland mansions
+
+# TODO: radii
 
 # Ocean monument near quad huts search
 mkdir /tmp/multifinder_monument
