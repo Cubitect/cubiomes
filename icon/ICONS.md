dbf5b86a579e0dada2eebbb62178e670f18964ac
Tom Schumm <fwiffo@gmail.com>
2018-07-05 14:52:00 -0700

    Start adding icons to rendermap.

---
+++ icon/ICONS.md
@@ -0,0 +1,6 @@
+About
+=====
+
+Icon images are copied from [Amidst](http://github.com/toolbox4minecraft/amidst)
+without modification. Amidst is also licensed under GNU General Public License
+v3.0.
--- rendermap.c
+++ rendermap.c
@@ -1,13 +1,17 @@
+#include "finders.h"
 #include "generator.h"
 #include "layers.h"
 
 #include <errno.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define DEFAULT_WIDTH 3840*2
 #define DEFAULT_HEIGHT DEFAULT_WIDTH*9/16
+#define ICON_SIZE 16
+#define ICON_SCALE 4
 
 void setBiomeColour(unsigned char biomeColour[256][3], int biome,
         unsigned char r, unsigned char g, unsigned char b)
@@ -122,7 +126,11 @@ void initBiomeColours(unsigned char biomeColours[256][3])
 void usage() {
     fprintf(stderr, "Usage:\n");
     fprintf(stderr, "    rendermap [seed] [filename]\n");
-    fprintf(stderr, "    rendermap [seed] [filename] [width] [height]\n");
+    fprintf(stderr, "    rendermap [seed] [filename] [width] [height]\n\n");
+    fprintf(stderr, "Do not inclde an extension in the filename. A .ppm file");
+    fprintf(stderr, "will be used as a simple intermediate format, then a\n");
+    fprintf(stderr, "command line will be provided to compose it with icons\n");
+    fprintf(stderr, "with imagemagick and save it as a .png.\n");
 }
 
 
@@ -146,13 +154,11 @@ void writePPMHeader(FILE *fp, int width, int height) {
 }
 
 
-void writeMap(int64_t seed, FILE *fp, int width, int height) {
+void writeMap(LayerStack *g, FILE *fp, int width, int height) {
     unsigned char biomeColors[256][3];
     initBiomeColours(biomeColors);
 
-    LayerStack g = setupGenerator();
-    Layer *fullRes = &g.layers[g.layerNum-1];
-    applySeed(&g, seed);
+    Layer *fullRes = &g->layers[g->layerNum-1];
     int *cache = allocCache(fullRes, width, 256);
     unsigned char pixelBuf[256*3];
 
@@ -174,14 +180,80 @@ void writeMap(int64_t seed, FILE *fp, int width, int height) {
             pixels += toWrite;
         }
     }
-    // TODO: print locations of
-    // spawn
-    // witch huts
+
+    free(cache);
+}
+
+
+void addIcon(char *icon, int width, int height, Pos pos, int scale) {
+    int iconSize = ICON_SIZE*scale;
+    int realX = pos.x + width/2 - iconSize/2;
+    int realZ = pos.z + height/2 - iconSize/2;
+
+    // Just ignore icons that are off the edge of the map.
+    if (realX < 0 || realZ < 0 ||
+            realX > width-iconSize || realZ > height-iconSize)
+        return;
+
+    printf("    \\( \"icon/%s.png\" -resize %d00%% \\) -geometry +%d+%d -composite \\\n",
+            icon, scale, realX, realZ);
+}
+
+
+int regionify(int val, int chunks) {
+    return (int)ceil((float)val / 16 / chunks);
+}
+
+
+int getBiomeAt(const LayerStack *g, const Pos pos, int *buf) {
+    genArea(&g->layers[g->layerNum-1], buf, pos.x, pos.z, 1, 1);
+    return buf[0];
+}
+
+
+void printCompositeCommand(LayerStack *g, int64_t seed, char *ppmfn, char *pngfn, int width, int height) {
+    Layer *fullRes = &g->layers[g->layerNum-1];
+    int *cache = allocCache(fullRes, 256, 256);
+
+    printf("convert \"%s\" -filter Point \\\n", ppmfn);
+    Pos spawn = getSpawn(g, cache, seed);
+    addIcon("spawn", width, height, spawn, ICON_SCALE);
+
+    int rX = regionify(width/2, 32);
+    int rZ = regionify(height/2, 32);
+    for (int z=-rZ; z<rZ; z++) {
+        for (int x=-rX; x<rX; x++) {
+            Pos pos;
+            int biomeAt;
+
+            pos = getStructurePos(DESERT_PYRAMID_SEED, seed, x, z);
+            biomeAt = getBiomeAt(g, pos, cache);
+            if (biomeAt == desert || biomeAt == desertHills)
+                addIcon("desert", width, height, pos, ICON_SCALE);
+
+            pos = getStructurePos(IGLOO_SEED, seed, x, z);
+            biomeAt = getBiomeAt(g, pos, cache);
+            if (biomeAt == icePlains || biomeAt == coldTaiga)
+                addIcon("igloo", width, height, pos, ICON_SCALE);
+
+            pos = getStructurePos(JUNGLE_PYRAMID_SEED, seed, x, z);
+            biomeAt = getBiomeAt(g, pos, cache);
+            if (biomeAt == jungle || biomeAt == jungleHills)
+                addIcon("jungle", width, height, pos, ICON_SCALE);
+
+            pos = getStructurePos(SWAMP_HUT_SEED, seed, x, z);
+            biomeAt = getBiomeAt(g, pos, cache);
+            if (biomeAt == swampland)
+                addIcon("witch", width, height, pos, ICON_SCALE);
+        }
+    }
+    // TODO:
     // ocean monuments
     // strongholds
     // woodland mansions
-    // desert temples, igloos, jungle temples
     // villages
+    // Add seed text overlay?
+    printf("    \"%s\"\n", pngfn);
 
     free(cache);
 }
@@ -218,15 +290,32 @@ int main(int argc, char *argv[]) {
         height = DEFAULT_HEIGHT;
     }
 
-    FILE *fp = fopen(argv[2], "w");
+    char ppmfn[256];
+    char pngfn[256];
+    if (strlen(argv[2]) > 250) {
+        fprintf(stderr, "Filename too long.\n");
+        exit(-1);
+    }
+    snprintf(ppmfn, 256, "%s.ppm", argv[2]);
+    snprintf(pngfn, 256, "%s.png", argv[2]);
+
+    FILE *fp = fopen(ppmfn, "w");
     if (fp == NULL) {
         fprintf(stderr, "Could not open file %s for writing.\n", argv[2]);
         exit(-1);
     }
 
+    // Write the base map as a PPM file
     initBiomes();
-    writeMap(seed, fp, width, height);
+    LayerStack g = setupGenerator();
+    applySeed(&g, seed);
+    writeMap(&g, fp, width, height);
 
     fclose(fp);
+
+    // Write a imagemagick command to compose the map with icons and convert
+    // to a .png file.
+    printCompositeCommand(&g, seed, ppmfn, pngfn, width, height);
+
     return 0;
 }
