833e13516dc7b140b97366ed4f664d12dc857aae
Kai Sandstrom <ksandstrom2@gmail.com>
2023-01-08 17:32:59 -0500

    Added new entries in MCVersion and LayerID enumerations

--- layers.h
+++ layers.h
@@ -10,7 +10,7 @@
 /* Minecraft versions */
 enum MCVersion
 {
-    MC_1_0, // <=1.0 Experimental!
+    MC_B1_8, MC_1_0, // <=1.0 Experimental!
     MC_1_1,  MC_1_2,  MC_1_3,  MC_1_4,  MC_1_5,  MC_1_6,
     MC_1_7,  MC_1_8,  MC_1_9,  MC_1_10, MC_1_11, MC_1_12,
     MC_1_13, MC_1_14, MC_1_15, MC_1_16, MC_1_17, MC_1_18,
@@ -157,6 +157,8 @@ enum LayerId
 {
     // new                  [[deprecated]]
     L_CONTINENT_4096 = 0,   L_ISLAND_4096 = L_CONTINENT_4096,
+    L_ZOOM_4096,                                                    // b1.8
+    L_LAND_4096,                                                    // b1.8
     L_ZOOM_2048,
     L_LAND_2048,            L_ADD_ISLAND_2048 = L_LAND_2048,
     L_ZOOM_1024,
