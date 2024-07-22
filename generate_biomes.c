55dda1ac5a873277ba5d3408ea3720d01df82e54
lorenzm-vacationme <lorenz@vacationme.net>
2024-07-22 20:08:02 +0800

    fix

--- generate_biomes.c
+++ generate_biomes.c
@@ -86,7 +86,7 @@ int main(int argc, char *argv[]) {
     Range r;
     r.scale = 16;         // Scale for biome coordinates
     r.x = -60; r.z = -60; // Starting coordinates
-    r.sx = 800; r.sz = 480; // Size of the area to generate
+    r.sx = 300; r.sz = 100; // Size of the area to generate
     r.y = 15; r.sy = 1;     // y and sy are typically not used in 2D generation
 
     // Allocate memory for storing biome IDs
