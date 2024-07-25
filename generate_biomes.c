80566f35d1fac16f58f84309229d17b028822400
lorenzm-vacationme <lorenz@vacationme.net>
2024-07-25 18:29:13 +0800

    fix path remote

--- generate_biomes.c
+++ generate_biomes.c
@@ -72,8 +72,8 @@ int main(int argc, char *argv[]) {
     biomesToImage(rgb, biomeColors, biomeIds, r.sx, r.sz, pix4cell, 2);
 
     // Define the output directory relative to the container's working directory
-    // const char *dirUrl = "/var/www/gme-backend/storage/app/public/images/seeds"; //remote file path
-    const char *dirUrl = "/var/www/storage/app/public/images/seeds"; //local file path
+    const char *dirUrl = "/var/www/gme-backend/storage/app/public/images/seeds"; //remote file path
+    // const char *dirUrl = "/var/www/storage/app/public/images/seeds"; //local file path
 
     // Ensure the directory exists
     if (createDir(dirUrl) != 0) {
